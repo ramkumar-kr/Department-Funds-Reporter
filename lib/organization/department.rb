a33bf1184787e53350126a28d2a57efef84dffbd
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-06-10 17:45:56 +0530

    Removed equality and hash

--- lib/organization/department.rb
+++ lib/organization/department.rb
@@ -1,23 +1,11 @@
 # Represents a division in an organization
 class Organization::Department
-	attr_reader :name, :total_funds, :sub_departments
+	
 	def initialize(department_name, total_funds = 0.0, sub_departments = [])
 		@name = department_name
 		@total_funds = total_funds
 		@sub_departments = sub_departments
 	end
-
-	def ==(other_object)
-		return false unless self.class == other_object.class
-		self.name == other_object.name && self.total_funds == other_object.total_funds && self.sub_departments == other_object.sub_departments
-	end
-
-	def hash
-		[name, total_funds].hash
-	end
-	
-	alias :eql? :==
-
 	def get_cash
 		sum = 0.0
 		if @sub_departments.length == 0
--- spec/organization/department_spec.rb
+++ spec/organization/department_spec.rb
@@ -1,41 +1,7 @@
 require "spec_helper"
 
 describe Organization::Department do
-  context 'Equality' do
-  	it "should be equal to itself" do
-  		department1 = Organization::Department.new("Finance", 10000)
-  		expect(department1).to eq(department1)
-  	end
-
-  	it "should be equal to another Department object with same name and cash" do
-  		department1 = Organization::Department.new("Finance", 10000)
-  		department2 = Organization::Department.new("Finance", 10000)
-  		expect(department1).to eq(department2)
-  	end
-
-  	it "should be equal to another Department object with same name, cash and sub_departments" do  		
-  		sub_department1 = Organization::Department.new("Finance_Delhi", 10000)
-  		sub_department2 = Organization::Department.new("Finance_Indore", 10000)
-  		sub_department3 = Organization::Department.new("Finance_Mumbai", 10000)
-  		department1 = Organization::Department.new("Finance", [sub_department1, sub_department2, sub_department3])
-  		department2 = Organization::Department.new("Finance", [sub_department1, sub_department2, sub_department3])
-  		expect(department1).to eq(department2)
-  	end
-
-  	it "should not be equal to another Department object with different name and cash" do
-  		department1 = Organization::Department.new("Finance", 10000)
-  		department2 = Organization::Department.new("Engineering", 10001)
-  		expect(department1).to_not eq(department2)
-  	end
-
-  	it "should not be equal to nil" do
-  		department1 = Organization::Department.new("Finance", 10000)
-  		department2 = nil
-  		expect(department1).to_not eq(department2)
-  	end
-  end
-
-
+ 
   context 'Get total funds' do
   	it "should return total funds of a Department as a sum of funds in each of it's sub departments" do
   		sub_department1 = Organization::Department.new("Finance_Delhi", 10000)
@@ -43,15 +9,6 @@ describe Organization::Department do
   		sub_department3 = Organization::Department.new("Finance_Mumbai", 10000)
   		department = Organization::Department.new("Finance",0, [sub_department1, sub_department2, sub_department3])
   		expect(department.get_cash).to eq(30000.0)
-  	end
-  	
-  end
-  context 'Hash' do
-  	it "should have the same hash with another object with same name and cash" do
-  		department1 = Organization::Department.new("Finance", 10000)
-  		department2 = Organization::Department.new("Finance", 10000)
-  		expect(department1.hash).to eq(department2.hash)
-  	end
-  end
-
+  	end	
+  end   
 end
