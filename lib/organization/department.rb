2e3e073463bdf201e913b086fd7754e4931f1588
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-06-10 17:07:31 +0530

    Added get_cash method

--- lib/organization/department.rb
+++ lib/organization/department.rb
@@ -1,7 +1,7 @@
 # Represents a division in an organization
 class Organization::Department
 	attr_reader :name, :total_funds, :sub_departments
-	def initialize(department_name, total_funds, sub_departments = [])
+	def initialize(department_name, total_funds = 0.0, sub_departments = [])
 		@name = department_name
 		@total_funds = total_funds
 		@sub_departments = sub_departments
@@ -15,7 +15,16 @@ class Organization::Department
 	def hash
 		[name, total_funds].hash
 	end
-
+	
 	alias :eql? :==
+
+	def get_cash
+		sum = 0.0
+		if @sub_departments.length == 0
+			return @total_funds
+		end
+		@sub_departments.each { |e| sum = sum + e.get_cash  }
+		sum
+	end
 	
 end
--- spec/organization/department_spec.rb
+++ spec/organization/department_spec.rb
@@ -13,13 +13,12 @@ describe Organization::Department do
   		expect(department1).to eq(department2)
   	end
 
-  	it "should be equal to another Department object with same name, cash and sub_departments" do
-  		
+  	it "should be equal to another Department object with same name, cash and sub_departments" do  		
   		sub_department1 = Organization::Department.new("Finance_Delhi", 10000)
   		sub_department2 = Organization::Department.new("Finance_Indore", 10000)
   		sub_department3 = Organization::Department.new("Finance_Mumbai", 10000)
-  		department1 = Organization::Department.new("Finance", 10000, [sub_department1, sub_department2, sub_department3])
-  		department2 = Organization::Department.new("Finance", 10000, [sub_department1, sub_department2, sub_department3])
+  		department1 = Organization::Department.new("Finance", [sub_department1, sub_department2, sub_department3])
+  		department2 = Organization::Department.new("Finance", [sub_department1, sub_department2, sub_department3])
   		expect(department1).to eq(department2)
   	end
 
@@ -36,6 +35,17 @@ describe Organization::Department do
   	end
   end
 
+
+  context 'Get total funds' do
+  	it "should return total funds of a Department as a sum of funds in each of it's sub departments" do
+  		sub_department1 = Organization::Department.new("Finance_Delhi", 10000)
+  		sub_department2 = Organization::Department.new("Finance_Indore", 10000)
+  		sub_department3 = Organization::Department.new("Finance_Mumbai", 10000)
+  		department = Organization::Department.new("Finance",0, [sub_department1, sub_department2, sub_department3])
+  		expect(department.get_cash).to eq(30000.0)
+  	end
+  	
+  end
   context 'Hash' do
   	it "should have the same hash with another object with same name and cash" do
   		department1 = Organization::Department.new("Finance", 10000)
