beb023b465c3712f607c9e5f999675897185dfb3
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-06-11 11:26:34 +0530

    Configured Managerial department specs to use factory girl

--- spec/factories/managerial_department.rb
+++ spec/factories/managerial_department.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
-	factory :managerial, class: Organization::ManagerialDepartment do
+	factory :managerial_department, class: Organization::ManagerialDepartment do
 		name "Untitled"
 		sub_departments []
+		initialize_with { new(name, sub_departments) }
 	end
 end
